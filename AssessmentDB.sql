919c03cab454781f0977230707a485215efec592
AlexThomasWebElevate <alex.thomas@webelevate.ie>
2016-02-13 20:36:33 +0000

    Update AssessmentDB.sql

--- AssessmentDB.sql
+++ AssessmentDB.sql
@@ -19,7 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `AssessmentDB`
 --
-
+CREATE DATABASE IF NOT EXISTS `AssessmentDB` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `AssessmentDB`;
 -- --------------------------------------------------------
 
 --
